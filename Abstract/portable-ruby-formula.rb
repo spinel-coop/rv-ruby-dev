01a87be9e9f47eae0cecb103431879f9e604f842
André Arko <andre@arko.net>
2025-09-05 17:13:56 -0700

    handle both ruby 3.3 and 3.4 hash inspect

--- Abstract/portable-ruby-formula.rb
+++ Abstract/portable-ruby-formula.rb
@@ -169,7 +169,7 @@ class PortableRubyFormula < PortableFormula
       shell_output("#{ruby} -rzlib -e 'puts Zlib.crc32(\"test\")'").chomp
     assert_equal " \t\n`><=;|&{(",
       shell_output("#{ruby} -rreadline -e 'puts Readline.basic_word_break_characters'").chomp
-    assert_equal '{"a" => "b"}',
+    assert_equal (version < "3.4.0" ? '{"a"=>"b"}' : '{"a" => "b"}'),
       shell_output("#{ruby} -ryaml -e 'puts YAML.load(\"a: b\")'").chomp
     assert_equal "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
       shell_output("#{ruby} -ropenssl -e 'puts OpenSSL::Digest::SHA256.hexdigest(\"\")'").chomp
