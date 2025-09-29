cad8a7ccd2ae22a498b27cc628cecf14ae38db1b
André Arko <andre@arko.net>
2025-09-29 19:58:14 +0900

    add 3.3.8

---
+++ Formula/rv-ruby@3.3.8.rb
@@ -0,0 +1,6 @@
+require File.expand_path("../Abstract/rv-ruby-33", __dir__)
+
+class RvRubyAT338 < RvRuby33
+  url "https://cache.ruby-lang.org/pub/ruby/3.3/ruby-3.3.8.tar.gz"
+  sha256 "5ae28a87a59a3e4ad66bc2931d232dbab953d0aa8f6baf3bc4f8f80977c89cab"
+end
