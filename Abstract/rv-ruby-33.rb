1fce5f191dfcc3faeb6f24030e98d19a52e2548f
André Arko <andre@arko.net>
2025-09-30 07:42:56 -0700

    disable non-yjit replacements if yjit is on

--- Abstract/rv-ruby-33.rb
+++ Abstract/rv-ruby-33.rb
@@ -158,14 +158,16 @@ class RvRuby33 < Formula
     abi_arch = `#{bin}/ruby -rrbconfig -e 'print RbConfig::CONFIG["arch"]'`
 
     if OS.linux?
-      # Don't restrict to a specific GCC compiler binary we used (e.g. gcc-5).
-      inreplace lib/"ruby/#{abi_version}/#{abi_arch}/rbconfig.rb" do |s|
-        s.gsub! ENV.cxx, "c++"
-        s.gsub! ENV.cc, "cc"
-        # Change e.g. `CONFIG["AR"] = "gcc-ar-11"` to `CONFIG["AR"] = "ar"`
-        s.gsub!(/(CONFIG\[".+"\] = )"gcc-(.*)-\d+"/, '\\1"\\2"')
-        # C++ compiler might have been disabled because we break it with glibc@* builds
-        s.sub!(/(CONFIG\["CXX"\] = )"false"/, '\\1"c++"')
+      if build.without? "yjit"
+        # Don't restrict to a specific GCC compiler binary we used (e.g. gcc-5).
+        inreplace lib/"ruby/#{abi_version}/#{abi_arch}/rbconfig.rb" do |s|
+          s.gsub! ENV.cxx, "c++"
+          s.gsub! ENV.cc, "cc"
+          # Change e.g. `CONFIG["AR"] = "gcc-ar-11"` to `CONFIG["AR"] = "ar"`
+          s.gsub!(/(CONFIG\[".+"\] = )"gcc-(.*)-\d+"/, '\\1"\\2"')
+          # C++ compiler might have been disabled because we break it with glibc@* builds
+          s.sub!(/(CONFIG\["CXX"\] = )"false"/, '\\1"c++"')
+        end
       end
 
       # Ship libcrypt.a so that building native gems doesn't need system libcrypt installed.
