381f1bd4b2ee3370c4fa5358ac7e036f1850895f
André Arko <andre@arko.net>
2025-09-30 10:43:13 -0700

    fix the version match check

--- Abstract/rv-ruby-33.rb
+++ Abstract/rv-ruby-33.rb
@@ -101,7 +101,7 @@ class RvRuby33 < Formula
       --disable-dependency-tracking
     ]
 
-    if ENV.key?("HOMEBREW_BASERUBY") && %x[#{ENV["HOMEBREW_BASERUBY"]} -v].includes(version.to_s)
+    if ENV.key?("HOMEBREW_BASERUBY") && %x[#{ENV["HOMEBREW_BASERUBY"]} -v] =~ /#{Regex.escape(version)}/
       args += %W[--with-baseruby=#{ENV["HOMEBREW_BASERUBY"]}]
     else
       odie "HOMEBREW_BASERUBY must contain the path to a ruby #{version} executable"
