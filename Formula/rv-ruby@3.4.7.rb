c0c22efccff106b8a28f13dfbe67dacb96f39498
André Arko <andre@arko.net>
2025-10-08 11:05:14 -0600

    add ruby 3.4.7

---
+++ Formula/rv-ruby@3.4.7.rb
@@ -0,0 +1,6 @@
+require File.expand_path("../Abstract/rv-ruby-34", __dir__)
+
+class RvRubyAT346 < RvRuby34
+  url "https://cache.ruby-lang.org/pub/ruby/3.4/ruby-3.4.7.tar.gz"
+  sha256 "23815a6d095696f7919090fdc3e2f9459b2c83d57224b2e446ce1f5f7333ef36"
+end
