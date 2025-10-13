617d373279d6675db6b35a18f3af0895ae1b02e5
André Arko <andre@arko.net>
2025-10-13 19:56:39 +0900

    remove 3.2.0 since it doesn't work

--- Formula/rv-ruby@3.2.0.rb
+++
@@ -1,6 +0,0 @@
-require File.expand_path("../Abstract/rv-ruby-32", __dir__)
-
-class RvRubyAT320 < RvRuby32
-  url "https://cache.ruby-lang.org/pub/ruby/3.2/ruby-3.2.0.tar.xz"
-  sha256 "d2f4577306e6dd932259693233141e5c3ec13622c95b75996541b8d5b68b28b4"
-end
