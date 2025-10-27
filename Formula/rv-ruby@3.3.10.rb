8b1b37d9e5599420bc62288fc97feac84d082bc5
Mick Montorier-Aberman <mick_montorier-aberman@manda.fr>
2025-10-27 10:05:03 +0100

    Formula for Ruby 3.3.10

---
+++ Formula/rv-ruby@3.3.10.rb
@@ -0,0 +1,6 @@
+require File.expand_path("../Abstract/rv-ruby-33", __dir__)
+
+class RvRubyAT3310 < RvRuby33
+  url "https://cache.ruby-lang.org/pub/ruby/3.3/ruby-3.3.10.tar.gz"
+  sha256 "b555baa467a306cfc8e6c6ed24d0d27b27e9a1bed1d91d95509859eac6b0e928"
+end
