07e794e43f6149d238f7545176b5bc1d4cddad49
André Arko <andre@arko.net>
2025-11-22 12:19:51 -0800

    remove now-unused no-op option

--- Formula/rv-ruby@0.49.rb
+++ Formula/rv-ruby@0.49.rb
@@ -10,8 +10,6 @@ class RvRubyAT049 < Formula
   sha256 "88e011f60c6bb532c8e0787506a5ed518f31288bce749e1eb868febe290d83be"
   license "ruby"
 
-  option "with-yjit", "Build Ruby with YJIT" # no-op for compatibility with other formulas
-
   depends_on "bison" => :build
 
   def install
