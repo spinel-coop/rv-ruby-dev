7033ee90f46db6716d8bbb0ced95288aaa8123cf
André Arko <andre@arko.net>
2026-01-12 21:45:37 -0800

    Ruby 4.0.1

---
+++ Formula/rv-ruby@4.0.1.rb
@@ -0,0 +1,6 @@
+require File.expand_path("../Abstract/rv-ruby", __dir__)
+
+class RvRubyAT401 < RvRuby
+  url "https://cache.ruby-lang.org/pub/ruby/4.0/ruby-4.0.1.tar.gz"
+  sha256 "3924be2d05db30f4e35f859bf028be85f4b7dd01714142fd823e4af5de2faf9d"
+end
