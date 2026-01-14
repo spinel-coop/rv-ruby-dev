9c0d62e25fff2c6360b44a88546eecdffba8edf1
André Arko <andre@arko.net>
2026-01-13 17:56:59 -0800

    Ruby 3.2.10

---
+++ Formula/rv-ruby@3.2.10.rb
@@ -0,0 +1,6 @@
+require File.expand_path("../Abstract/rv-ruby-32", __dir__)
+
+class RvRubyAT3210 < RvRuby32
+  url "https://cache.ruby-lang.org/pub/ruby/3.2/ruby-3.2.10.tar.gz"
+  sha256 "880acb05e08da8c559c56a13e512bae1b472da67c72ebb750c765f9c2134e689"
+end
