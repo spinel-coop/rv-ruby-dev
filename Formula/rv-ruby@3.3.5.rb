c9db56db3295d27342fe5f152d1623d43acbf826
André Arko <andre@arko.net>
2026-02-20 23:45:20 -0800

    remove the .lock files Ruby 3.3.5 leaves in bin/
    why? I have no idea. homebrew complains about it, though (correctly, in
    my opinion)

--- Formula/rv-ruby@3.3.5.rb
+++ Formula/rv-ruby@3.3.5.rb
@@ -3,4 +3,10 @@ require File.expand_path("../Abstract/rv-ruby-33", __dir__)
 class RvRubyAT335 < RvRuby33
   url "https://cache.ruby-lang.org/pub/ruby/3.3/ruby-3.3.5.tar.gz"
   sha256 "3781a3504222c2f26cb4b9eb9c1a12dbf4944d366ce24a9ff8cf99ecbce75196"
+
+  def install
+    super
+
+    bin.glob("*.lock").each(&:rmtree)
+  end
 end
