63083263859e34344ce41b12cb8074dac9d7bfad
André Arko <andre@arko.net>
2026-03-26 22:54:48 -0700

    ruby 3.2.11

---
+++ Formula/rv-ruby@3.2.11.rb
@@ -0,0 +1,6 @@
+require File.expand_path("../Abstract/rv-ruby-32", __dir__)
+
+class RvRubyAT3211 < RvRuby32
+  url "https://cache.ruby-lang.org/pub/ruby/3.2/ruby-3.2.11.tar.gz"
+  sha256 "b3eeabd6636f334531db3ffdc3229eb05e524740e6c84fdc043720573cf2f8b2"
+end
