68d6dd26d8323e7de98b681ffaa9fa33b0770e11
André Arko <andre@arko.net>
2026-04-17 23:22:43 -0700

    update rv-package to handle Homebrew/brew#22015

--- cmd/rv-package.rb
+++ cmd/rv-package.rb
@@ -46,12 +46,12 @@ module Homebrew
             # Install build deps (but not static-linked deps) from bottles, to save compilation time
             bottled_dep_allowlist = /\A(?:glibc@|linux-headers@|ruby@|rustup|autoconf|pkgconf|bison)/
             deps = Dependency.expand(Formula[name], cache_key: "rv-package-#{name}") do |_dependent, dep|
-              Dependency.prune if dep.test? || dep.optional?
-              Dependency.prune if dep.name == "rustup" && args.without_yjit?
+              next Dependable::PRUNE if dep.test? || dep.optional?
+              next Dependable::PRUNE if dep.name == "rustup" && args.without_yjit?
 
               next unless bottled_dep_allowlist.match?(dep.name)
 
-              Dependency.keep_but_prune_recursive_deps
+              Dependable::KEEP_BUT_PRUNE_RECURSIVE_DEPS
             end.map(&:name)
 
             bottled_deps, deps = deps.partition { |dep| bottled_dep_allowlist.match?(dep) }
